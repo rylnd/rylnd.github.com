d14bc2d5d3d1c9b19e36eff4f61a1538bc46fe9e
Ryland Herrick <ryland@bendyworks.com>
2013-06-15 22:05:25 -0500

    Site updated at 2013-06-16 03:05:25 UTC

--- javascripts/slash.js
+++ javascripts/slash.js
@@ -1,45 +1,27 @@
 (function($){
-	/* external.js */
-	var host = location.host;
 
-	$('a').on('click', function(e){
-		var href = $(this).attr('href'),
-			link = href.replace(/(https?:\/\/)(.*)\/(.*)/, '$2');
+	var mobilenav = $('#mobile-nav');
 
-		if (href.match('https?') && link != host){
-			window.open(href);
-			e.preventDefault();
+	$('html').click(function(){
+		mobilenav.find('.on').each(function(){
+			$(this).removeClass('on').next().hide();
+		});
+	});
+
+	mobilenav.on('click', '.menu .button', function(){
+		if (!$(this).hasClass('on')){
+			$(this).addClass('on').next().show();
+		} else {
+			$(this).removeClass('on').next().hide();
 		}
+	}).on('click', '.search .button', function(){
+		if (!$(this).hasClass('on')){
+			mobilenav.children('.menu').children().eq(0).removeClass('on').next().hide();
+			$(this).addClass('on').next().show().children().children().eq(0).focus();
+		} else {
+			$(this).removeClass('on').next().hide().children().children().eq(0).val('');
+		}
+	}).click(function(e){
+		e.stopPropagation();
 	});
-
-	/* navigation.js */
-	var appends = '<option>Menu</option>';
-
-	$('.menu .main > li').each(function(){
-		var link = $(this).children('a');
-		appends += '<option value="'+link.attr('href')+'">'+link.html()+'</option>';
-		$(this).find('li').each(function(){
-			var link = $(this).children('a');
-			appends += '<option value="'+link.attr('href')+'">- '+link.html()+'</option>';
-		});
-	});
-
-	$('nav.menu').append('<select>'+appends+'</select>').on('change', 'select', function(){
-		location.href = $(this).val();
-	});
-
-	/* caption.js */
-	$('.entry').each(function(i){
-		var _i = i;
-		$(this).find('img').each(function(){
-			var alt = $(this).attr('alt');
-
-			if (alt == '' || typeof alt == 'undefined'){
-				$(this).wrap('<a href="'+$(this).attr('src')+'" class="fancybox" rel="gallery'+_i+'" />');
-			} else {
-				$(this).after('<span class="caption">'+alt+'</span>').wrap('<a href="'+$(this).attr('src')+'" class="fancybox" title="'+alt+'" rel="gallery'+_i+'" />');
-			}
-		});
-	});
-	$('.fancybox').fancybox();
 })(jQuery);
